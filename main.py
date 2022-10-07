e895947c3f1590101666fa52849c066d9ade18cd
SMolletSchool <shmollet@cpp.edu>
2022-10-07 14:25:52 -0700

    Baseline basic stuff

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+CS3560.01, Group 6
+The Main driver for the Patient Scheduling Software.
+
+'''
+
+import classes
+import sys
+
+def main():
+    #code to check if command line args work, not sticking around in the future
+    print(sys.argv)
+
+if __name__ == "__main__":
+    main()
